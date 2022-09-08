ddad8c0f4345fcda9a4b139b5426a62c84b6dbd6
Jeremy Boggs <jeremy@virginia.edu>
2022-09-08 13:06:09 -0400

    Updates analytics tracking to Matomo

--- footer.php
+++ footer.php
@@ -68,23 +68,6 @@
              });
         </script>
     <?php endif; ?>
-
-
-
-    <!-- Google analytics - Need to check if this is still a valid account or if it needs updating -->
-    <script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-19425148-1']);
-      _gaq.push(['_setDomainName', '.lib.virginia.edu']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
+    <!-- Matomo --><script> var _paq = window._paq = window._paq || []; /* tracker methods like "setCustomDimension" should be called before "trackPageView" */ _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u="https://analytics.lib.virginia.edu/"; _paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '44']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s); })(); </script> <!-- End Matomo Code -->
 </body>
 </html>
